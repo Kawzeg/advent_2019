efaa2ff2db5de315e085b43b4903ccb7833fd472
Finn Günther <mail@kawzeg.com>
2019-12-03 17:35:44 +0100

    Day 3: Start Part 2

--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -144,7 +144,7 @@ fn spiral(origin: (i32, i32)) -> Spiral {
     }
 }
 
-fn main() -> io::Result<()> {
+fn part_1() -> io::Result<()> {
     let file = "./resources/input";
     let input = std::fs::read_to_string(file)?;
     //let input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"; //works
@@ -193,5 +193,9 @@ fn main() -> io::Result<()> {
         }
     }
 
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
     Ok(())
 }
