00b24e982d544691e2cb5487fa96db033610efab
Finn Günther <mail@kawzeg.com>
2019-12-04 17:29:51 +0100

    Day 4: Part 2

--- aoc04/src/main.rs
+++ aoc04/src/main.rs
@@ -1,14 +1,35 @@
 /// Two adjacent are the same
-
+/// But not part of a larger match
 fn adjacent_rule(candidate: String) -> bool {
     let mut prev = candidate.chars().next().unwrap();
+    let mut preliminary = false;
+    let mut needs_diff = false;
     for c in candidate[1..].chars() {
-        if prev == c {
+        // println!("{}, {}, prel: {}, needs_diff: {}", prev, c, preliminary, needs_diff);
+        if needs_diff && prev == c {
+            prev = c;
+            continue;
+        } else if needs_diff {
+            needs_diff = false;
+        }
+        if preliminary && prev != c {
             return true;
+        } else if preliminary {
+            preliminary = false;
+            needs_diff = true;
+            prev = c;
+            continue;
+        }
+        if prev == c {
+            preliminary = true;
         }
         prev = c;
     }
-    false
+    if preliminary {
+        true
+    } else {
+        false
+    }
 }
 
 fn increase_rule(candidate: String) -> bool {
@@ -30,6 +51,7 @@ fn main() {
     let mut matching = 0;
     for x in min..max {
         if adjacent_rule(x.to_string()) && increase_rule(x.to_string()) {
+            println!("Match: {}", x);
             matching += 1;
         }
     }
@@ -45,6 +67,10 @@ mod tests {
     #[test]
     fn adjacent_rule() {
         assert!(super::adjacent_rule("122345".to_string()));
+        assert!(super::adjacent_rule("111144".to_string()));
+        assert!(!super::adjacent_rule("111111".to_string()));
+        assert!(!super::adjacent_rule("111444".to_string()));
+        assert!(!super::adjacent_rule("111123".to_string()));
     }
 
     fn increase_rule() {
