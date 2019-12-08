d9c5a9df9b059b6141f2711db8e0bd4327da312b
Finn Günther <mail@kawzeg.com>
2019-12-08 17:57:08 +0100

    Day 8: Part 2

--- aoc08/src/main.rs
+++ aoc08/src/main.rs
@@ -1,10 +1,16 @@
 use std::io;
 
+const width: usize = 25;
+const height: usize = 6;
 
+fn copy_from(image: &[u32]) -> [u32;width] {
+    let mut arr = [0;width];
+    let bytes = &image[..arr.len()];
+    arr.copy_from_slice(bytes);
+    arr
+}
 
 fn main() -> io::Result<()> {
-    let width = 25;
-    let height = 6;
     let file = "./resources/input";
     let input = std::fs::read_to_string(file)?;
     let mut layers: Vec<Vec<u32>> = vec![];
@@ -38,6 +44,32 @@ fn main() -> io::Result<()> {
     let ones = target.iter().filter(|x|{**x==1}).count();
     let twos = target.iter().filter(|x|{**x==2}).count();
     println!("Solution: {}*{}={}", ones, twos, ones*twos);
+
+    let mut image : Vec<u32> = vec![];
+    for i in 0..width*height {
+        let mut pixel = 2;
+        for layer in &layers {
+            if layer[i] == 2 {
+                continue;
+            } else {
+                pixel = layer[i];
+                break;
+            }
+        }
+        image.push(pixel);
+    }
+
+    for x in 0..height {
+        let row: [u32;width] = copy_from(&image[x*width..x*width+width]);
+        for c in row.iter() {
+            if *c == 0 {
+                print!(" ");
+            } else {
+                print!("#");
+            }
+        }
+        print!("\n");
+    }
 
     Ok(())
 }
