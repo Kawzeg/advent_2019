24d34393b48dbf7e5f0c66e51351c2c75d16d66b
Finn Günther <mail@kawzeg.com>
2019-12-17 17:43:46 +0100

    Day 17: Part 2

--- aoc17/src/main.rs
+++ aoc17/src/main.rs
@@ -1,6 +1,20 @@
 use intcode::{intcode_from_file, run_with_io};
 use std::io;
 
+use std::io::prelude::*;
+
+fn pause() {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "Press any key to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
+}
+
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum Direction {
     North,
@@ -97,7 +111,7 @@ fn get_intersections(map: &Map<TileType>) -> Vec<(usize, usize)> {
     r
 }
 
-fn main() -> io::Result<()> {
+fn part1() -> io::Result<()> {
     let code = intcode_from_file("./resources/input")?;
     let out = run_with_io(&code, vec![]);
     for x in &out.output {
@@ -128,5 +142,57 @@ fn main() -> io::Result<()> {
     }
     println!("Alignment Parameters: {}", sum);
     println!("Hello, world!");
+    Ok(())
+}
+
+fn add_sequence(input: &mut Vec<i64>, seq: &str) {
+    for c in seq.as_bytes() {
+        input.push(*c as i64);
+    }
+    input.push('\n' as i64); // Newline
+}
+
+fn main() -> io::Result<()> {
+    let mut code = intcode_from_file("./resources/input")?;
+    code.data[0] = 2;
+    let out = run_with_io(&code, vec![]);
+    for x in &out.output {
+        print!("{}", std::char::from_u32(*x as u32).unwrap().to_string());
+    }
+    let path = "L,12,L,8,R,10,R,10,L,6,L,4,L,12,L,12,L,8,R,10,R,10,L,6,L,4,L,12,R,10,L,8,L,4,R,10,L,6,L,4,L,12,L,12,L,8,R,10,R,10,R,10,L,8,L,4,R,10,L,6,L,4,L,12,R,10,L,8,L,4,R,10";
+    println!("Path should be: {}", path);
+    let mainseq = "A,B,A,B,C,B,A,C,B,C";
+    let a = "L,12,L,8,R,10,R,10";
+    let b = "L,6,L,4,L,12";
+    let c = "R,10,L,8,L,4,R,10";
+
+    let mut input: Vec<i64> = vec![];
+    add_sequence(&mut input, mainseq);
+    add_sequence(&mut input, a);
+    add_sequence(&mut input, b);
+    add_sequence(&mut input, c);
+    let video_feed = 'n'; // 'y' or 'n' to select interactive video feed or not
+    input.push(video_feed as i64);
+    input.push('\n' as i64);
+    let out = run_with_io(&code, input);
+    let mut y = 0;
+    let mut reached_video = false;
+    for c in out.output {
+        if c == 10 {
+            y += 1;
+            if y > 55 && !reached_video {
+                reached_video = true;
+                y = 0;
+            }
+            if video_feed == 'y' && reached_video && y % 50 == 0 {
+                pause();
+            }
+        }
+        if c > 256 {
+            println!("Space dust collected: {}", c);
+            break;
+        }
+        print!("{}", std::char::from_u32(c as u32).unwrap().to_string());
+    }
     Ok(())
 }
