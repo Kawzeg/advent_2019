067c5704ab664a448311ab9f9d7fdea114183bab
Finn Günther <mail@kawzeg.com>
2019-12-18 17:03:22 +0100

    Day 18: Precompute reachability

--- aoc18/src/main.rs
+++ aoc18/src/main.rs
@@ -7,6 +7,7 @@ use std::io::prelude::*;
 use std::cmp::Ordering;
 
 const KEYS: &str = "abcdefghijklmnopqrstuvwxyz";
+const DOORS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 #[derive(Debug, Eq, Clone)]
 struct State {
@@ -54,37 +55,6 @@ struct Neighbours {
     south: Option<(i64, usize)>,
     east: Option<(i64, usize)>,
     west: Option<(i64, usize)>,
-}
-
-fn get_neighbours(map: &Map<i64>, i: usize) -> Neighbours {
-    let (x, y) = map.index_xy(i);
-    let mut r = Neighbours {
-        south: None,
-        north: None,
-        east: None,
-        west: None,
-    };
-    if x > 0 {
-        let wi = map.xy_index(x - 1, y);
-        let w = map.tiles[wi];
-        r.west = Some((w, wi));
-    }
-    if x < map.width - 1 {
-        let ei = map.xy_index(x + 1, y);
-        let e = map.tiles[ei];
-        r.east = Some((e, ei));
-    }
-    if y > 0 {
-        let ni = map.xy_index(x, y - 1);
-        let n = map.tiles[ni];
-        r.north = Some((n, ni));
-    }
-    if y < map.height - 1 {
-        let si = map.xy_index(x, y + 1);
-        let s = map.tiles[si];
-        r.south = Some((s, si));
-    }
-    r
 }
 
 fn is_wall(tile: char, keys: &Vec<char>) -> bool {
@@ -135,8 +105,9 @@ where
     }
 }
 
-fn build_dijkstra_map(map: &Map<char>, key: char) -> Map<i64> {
+fn build_dijkstra_map(map: &Map<char>, key: char) -> Map<(i64, Vec<char>)> {
     let mut to_visit: BinaryHeap<Vertex<usize>> = BinaryHeap::new();
+    let mut visited: Vec<(i64, Vec<char>)> = vec![(0, vec![]); map.tiles.len()];
     let mut distances = HashMap::new();
     let i = get_index(map, key).unwrap();
     to_visit.push(Vertex(0, i));
@@ -150,22 +121,24 @@ fn build_dijkstra_map(map: &Map<char>, key: char) -> Map<i64> {
                 .map_or(true, |&current| new_distance < current);
             if is_shorter {
                 distances.insert(n, new_distance);
+                let tile = map.tiles[n];
+                let mut new_doors = visited[i].1.clone();
+                if DOORS.contains(tile) {
+                    new_doors.push(tile);
+                }
+                visited[n] = (new_distance, new_doors);
                 to_visit.push(Vertex(new_distance, n));
             }
         }
     }
-    let mut tiles = vec![std::i64::MAX; map.tiles.len()];
-    for (i, cost) in distances {
-        tiles[i] = cost;
-    }
     Map {
         width: map.width,
         height: map.height,
-        tiles: tiles,
+        tiles: visited,
     }
 }
 
-fn build_dijkstra_maps(map: &Map<char>, keys: Vec<char>) -> HashMap<char, Map<i64>> {
+fn build_dijkstra_maps(map: &Map<char>, keys: Vec<char>) -> HashMap<char, Map<(i64, Vec<char>)>> {
     let mut r = HashMap::new();
     for key in keys {
         if let Some(_) = get_index(&map, key) {
@@ -173,35 +146,6 @@ fn build_dijkstra_maps(map: &Map<char>, keys: Vec<char>) -> HashMap<char, Map<i6
         }
     }
     r
-}
-
-fn dijkstra(map: &Map<char>, start: char, target: char, keys: &Vec<char>) -> Option<i64> {
-    if let None = get_index(map, target) {
-        return None;
-    }
-    let target_i = get_index(map, target).unwrap();
-    let mut to_visit: BinaryHeap<Vertex<usize>> = BinaryHeap::new();
-    let mut distances = HashMap::new();
-    let i = get_index(map, start).unwrap();
-    to_visit.push(Vertex(0, i));
-    distances.insert(i, 0);
-    while let Some(Vertex(cost, i)) = to_visit.pop() {
-        let neighbours = neighbour_list(map, i, keys);
-        for n in neighbours {
-            let new_distance = cost + 1;
-            let is_shorter = distances
-                .get(&n)
-                .map_or(true, |&current| new_distance < current);
-            if is_shorter {
-                distances.insert(n, new_distance);
-                to_visit.push(Vertex(new_distance, n));
-                if n == target_i {
-                    return Some(new_distance);
-                }
-            }
-        }
-    }
-    distances.get(&target_i).map(|x| *x)
 }
 
 fn display(map: &Map<char>) {
@@ -244,57 +188,24 @@ fn parse_map(input: String) -> Map<char> {
     }
 }
 
-fn get_position(map: &Map<char>, target: char) -> (usize, usize) {
-    map.index_xy(map.tiles.iter().position(|x| *x == target).unwrap())
-}
-
 fn get_index(map: &Map<char>, target: char) -> Option<usize> {
     map.tiles.iter().position(|x| *x == target)
 }
 
-fn build_dijks(map: &Map<char>, keys: Vec<char>, collected_keys: &Vec<char>) -> Vec<(char, i64)> {
-    let mut r = vec![];
-    for key in keys {
-        let d = dijkstra(map, '@', key, collected_keys);
-        if let Some(distance) = d {
-            r.push((key, distance));
-        }
-    }
-    r
-}
-
 fn is_reachable(
     map: &Map<char>,
-    d_map: &Map<i64>,
+    d_map: &Map<(i64, Vec<char>)>,
     start: usize,
     keys: &Vec<char>,
 ) -> bool {
-    let mut i = start;
-    loop {
-        let neighbours = neighbour_list(map, i, &KEYS.chars().collect());
-        if neighbours.len() == 0 {
-            return false;
-        }
-        let mut min = d_map.tiles[i];
-        for n in neighbours {
-            if d_map.tiles[n] < min {
-                i = n;
-                min = d_map.tiles[n];
-            }
-        }
-        if is_wall(map.tiles[i], keys) {
-            return false;
-        }
-        if d_map.tiles[i] == 0 {
-            return true;
-        }
-    }
+    let needed_keys: Vec<char> = d_map.tiles[start].1.iter().map(|&x| x.to_ascii_lowercase()).collect();
+    needed_keys.iter().all(|x| keys.contains(x))
 }
 
 fn fast_neighbours(
     map: &Map<char>,
     state: &State,
-    d_maps: &HashMap<char, Map<i64>>,
+    d_maps: &HashMap<char, Map<(i64, Vec<char>)>>,
 ) -> Vec<(State, i64)> {
     let mut r = vec![];
     let mut keys = vec![];
@@ -332,103 +243,10 @@ fn fast_neighbours(
                 keys: new_keys,
                 i: new_index,
             };
-            r.push((new_state, distances.tiles[state.i]));
+            r.push((new_state, distances.tiles[state.i].0));
         }
     }
     r
-}
-
-fn optimize(map: &Map<char>, state: &State) -> Vec<(State, i64)> {
-    let mut r = vec![];
-    let mut keys = vec![];
-    let mut map = map.clone();
-    let i = get_index(&map, '@').unwrap();
-    map.tiles[i] = '.';
-    map.tiles[state.i] = '@';
-    for key in KEYS.chars() {
-        if !state.keys.contains(&key) {
-            keys.push(key);
-        }
-    }
-
-    //println!("Building dijkstras");
-    let distances = build_dijks(&map, keys, &state.keys);
-    //println!("Keys: {:?}", distances);
-
-    for (key, d) in distances {
-        let new_index = get_index(&map, key).unwrap();
-        let mut new_keys = state.keys.clone();
-        new_keys.push(key);
-        //new_keys.sort();
-        let mut new_map = map.clone();
-        let door = key.to_ascii_uppercase();
-        let door_index = get_index(&map, door);
-        new_map.tiles[new_index] = '@';
-        match door_index {
-            Some(i) => new_map.tiles[i] = '.',
-            _ => {}
-        }
-        new_map.tiles[state.i] = '.';
-        let new_state = State {
-            keys: new_keys,
-            i: new_index,
-        };
-        r.push((new_state, d));
-    }
-    //pause();
-
-    r
-}
-
-fn slow() -> io::Result<()> {
-    let file = "./resources/input";
-    let input = std::fs::read_to_string(file)?;
-
-    let map = parse_map(input);
-    display(&map);
-    let initial_state = State {
-        keys: vec![],
-        i: get_index(&map, '@').unwrap(),
-    };
-    let mut distances: HashMap<State, i64> = HashMap::new();
-    let mut to_visit: BinaryHeap<Vertex<State>> = BinaryHeap::new();
-    to_visit.push(Vertex(0, initial_state));
-    let num_keys = 26;
-    let mut lowest_path = vec![];
-    let mut lowest_cost = std::i64::MAX;
-    'outer: while let Some(Vertex(cost, state)) = to_visit.pop() {
-        println!("Cost: {:?}, Keys: {:?}", cost, state.keys);
-        if cost > lowest_cost {
-            break;
-        }
-        let neighbours = optimize(&map, &state);
-        for (n, new_cost) in neighbours {
-            //println!("New cost: {} to {:?}", new_cost, n);
-            let new_distance = cost + new_cost;
-            //println!("State: {:?}", n.keys);
-            //println!("Current: {:?}", distances.get(&n));
-            //println!("New: {:?}", new_distance);
-            let is_shorter = distances
-                .get(&n)
-                .map_or(true, |&current| new_distance < current);
-            //pause();
-            if is_shorter {
-                distances.insert(n.clone(), new_distance);
-                //println!("Pushing {} steps to {:?}", new_distance, n);
-                if n.keys.len() == num_keys {
-                    if new_distance < lowest_cost {
-                        lowest_cost = new_distance;
-                        lowest_path = n.keys.clone();
-                    }
-                }
-                to_visit.push(Vertex(new_distance, n));
-            }
-        }
-    }
-    println!("DONE");
-    println!("{} steps to {:?}", lowest_cost, lowest_path);
-    println!("Hello, world!");
-    Ok(())
 }
 
 fn main() -> io::Result<()> {
